57db2839aee26e6c0333add2bfe2b8c3121c01b5
masahiro.sanya <masahiro.sanya@uni.iret.co.jp>
2023-03-16 11:24:20 +0900

    stgブランチを作成

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,27 @@ module.exports = {
     env: {
       "browser": true,
       "jquery": true,
-      "browser": true,
       "es6": true,
       "node": true,
     },
+    plugins: [
+      '@typescript-eslint', // ESLintのTypeScriptプラグインのルールを適用できる様にする（/eslint-pluginは省略可）
+      'prettier'
+    ],
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
+        'plugin:@typescript-eslint/eslint-recommended', // eslint:recommendedに含まれるルールを型チェックでカバーできるものは無効化
+        'plugin:@typescript-eslint/recommended', // 型チェックが不要なルールを適用
+        'plugin:@typescript-eslint/recommended-requiring-type-checking', // 型チェックが必要なルールを適用
+        'prettier/@typescript-eslint', // Prettierでカバーできるルールを無効化
+        'plugin:prettier/recommended' // Prettierのお勧めルールセットを適用
     ],
     rules: {
       // …
      'react/react-in-jsx-scope': 'off',
     },
-    parser: "babel-eslint",
+    parser: "@typescript-eslint/parser",
     parserOptions: {
       sourceType: "module",
       ecmaVersion: 2015,
